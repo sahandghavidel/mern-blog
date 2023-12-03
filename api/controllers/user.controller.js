f64218a799299bf5e206abbd7f2cc85c670babce
Sahand Ghavidel <sahand.gha@gmail.com>
2023-12-03 08:36:43 +0700

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  res.json({ message: 'API is working!' });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -18,3 +19,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRoutes);
