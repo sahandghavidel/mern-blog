0a39d16155338e3119c62cf275e60e73622f05d0
Udari Dayarathne <it20056452@my.sliit.lk>
2024-03-05 15:22:06 +0530

    1

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,28 +4,37 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            Our Story
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+            Our story begins with Sir Andrew Caldecott, the then British Governor of Ceylon, 
+            who declared open the doors of the first state-owned commercial bank 
+            on August 1, 1939, in Fort, Colombo
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+            Since our inception, we have consistently engaged with Sri Lankans from all walks of life 
+            in a committed effort to create value for all stakeholders, with the objective of 
+            being the “preferred financial partner” for our customers.
+            With the largest network of touchpoints across the island, 
+            leveraged on a backbone of future-ready technologies, 
+            we strive to provide maximum convenience in access to financial services 
+            to our customers.
             </p>
 
             <p>
-              We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
-              each other grow and improve.
+            In creating greater value BOC established its international footprint 
+            with the establishment of overseas branches in key locations. 
+            Namely: London, UK, Male and Hulhumalé in the Maldives, Chennai, India, and the Seychelles, 
+            that are a part of Sri Lanka’s largest global network of overseas correspondent banks and exchange companies
+            </p>
+
+            <p>
+            As Sri Lanka’s premier financial services organisation with over eight decades of leadership 
+            in opening up a world of opportunity for our stakeholders and the nation, 
+            we are delighted to be the No. 1 bank in Sri Lanka that drives growth and prosperity for 
+            all citizens
             </p>
           </div>
         </div>
