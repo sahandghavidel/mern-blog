2284d3cd358b718aa78e6ed74b9b423dc26d6843
Szymon Pozniewski <Shhmonp@gmail.com>
2024-03-25 18:37:56 -0400

    fix sort by latest/newest posts in getposts function inside post controller
    You might have noticed that the sort by latest/newest doesn't work in our search page. It is due to a small typo in the post controller getposts function. Instead of "order" we are actually passing "sort" from our search page.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -30,7 +30,7 @@ export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
