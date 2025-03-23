dfc27d34e1c852bc10ee1719f4e643255a1baad2
abhijeet <abhijeettrivedi3064@gmail.com>
2025-03-23 23:57:53 +0530

    Description of changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3008',
         secure: false,
       },
     },
